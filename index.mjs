d765195cb6692a0b1c720f082825299ae7b74575
Hamzaali565 <ha6153806@gmail.com>
2023-02-22 00:08:06 -0800

    backend completed

--- index.mjs
+++ index.mjs
@@ -21,6 +21,8 @@ app.use(
   })
 );
 
+const SECRET = process.env.SECRET || "topsecret";
+
 // ---UserSchema---//
 const userSchema = new mongoose.Schema({
   fullName: { type: String },
@@ -42,14 +44,14 @@ const productSchema = new mongoose.Schema({
   unitPrice: { type: String, required: true },
   createdOn: { type: Date, default: Date.now },
 });
-export const productModel = mongoose.model("product", productSchema);
+const productModel = mongoose.model("product", productSchema);
 //--Category Api --//
 const categorySchema = new mongoose.Schema({
   image: { type: String, required: true },
   categoryName: { type: String, required: true },
   createdOn: { type: Date, default: Date.now },
 });
-export const categoryModel = mongoose.model("category", categorySchema);
+const categoryModel = mongoose.model("category", categorySchema);
 // ---Costumer Order Api--//
 const costumerOrderSchema = new mongoose.Schema({
   ownerName: { type: String, required: true },
@@ -59,12 +61,157 @@ const costumerOrderSchema = new mongoose.Schema({
   phoneNumber: { type: String, required: true },
   address: { type: String, required: true },
 });
-export const costumerOrderModel = mongoose.model(
-  "OrderSchema",
-  costumerOrderSchema
-);
+const costumerOrderModel = mongoose.model("OrderSchema", costumerOrderSchema);
+// --------user SignUP 1--------//
+app.post("/api/v1/signup", (req, res) => {
+  let body = req.body;
 
-//----- ADD PRODUCT API (ADMIN) ------///
+  if (!body.fullName || !body.email || !body.contact || !body.password) {
+    res.status(400).send({ message: "All Fields are Required" });
+    return;
+  }
+
+  req.body.email = req.body.email.toLowerCase();
+
+  // check if user already exist // query email user
+  userModel.findOne({ email: body.email }, (err, user) => {
+    if (!err) {
+      console.log("user: ", user);
+
+      if (user) {
+        // user already exist
+        console.log("user already exist: ", user);
+        res
+          .status(400)
+          .send({ message: "user already exist please try a different email" });
+        return;
+      } else {
+        // user not already exist
+
+        // bcrypt hash
+        stringToHash(body.password).then((hashString) => {
+          userModel.create(
+            {
+              fullName: body.fullName,
+              email: body.email,
+              password: hashString,
+              contact: body.contact,
+            },
+            (err, result) => {
+              if (!err) {
+                console.log("data saved: ", result);
+                res.status(201).send({ message: "user is created" });
+              } else {
+                console.log("db error: ", err);
+                res
+                  .status(500)
+                  .send({ message: "User Not Created. Please try again" });
+              }
+            }
+          );
+        });
+      }
+    } else {
+      console.log("db error: ", err);
+      res.status(500).send({ message: "db error in query" });
+      return;
+    }
+  });
+});
+// ---------------------------//
+
+//----------user Login 2----------//
+app.post("/api/v1/login", (req, res) => {
+  let body = req.body;
+
+  if (!body.email || !body.password) {
+    // null check - undefined, "", 0 , false, null , NaN
+    res.status(400).send({
+      message: "All Fields are Required",
+    });
+    return;
+  }
+
+  body.email = body.email.toLowerCase();
+  // check if user exist
+  userModel.findOne(
+    { email: body.email },
+    "fullName email password contact",
+    (err, data) => {
+      if (!err) {
+        console.log("data: ", data);
+
+        if (data) {
+          // user found
+          varifyHash(body.password, data.password).then((isMatched) => {
+            console.log("isMatched: ", isMatched);
+
+            if (isMatched) {
+              const token = jwt.sign(
+                {
+                  _id: data._id,
+                  email: data.email,
+                  iat: Math.floor(Date.now() / 1000) - 30,
+                  exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+                },
+                SECRET
+              );
+
+              console.log("token: ", token);
+
+              res.cookie("Token", token, {
+                maxAge: 86_400_000,
+                httpOnly: true,
+                sameSite: "none",
+                secure: true,
+              });
+
+              res.send({
+                message: "login successful",
+                profile: {
+                  fullName: data.fullName,
+                  email: data.email,
+                  contact: data.contact,
+                  _id: data._id,
+                },
+              });
+              return;
+            } else {
+              console.log("password did not match");
+              res.status(401).send({ message: "Incorrect email or password" });
+              return;
+            }
+          });
+        } else {
+          // user not already exist
+          console.log("user not found");
+          res.status(401).send({ message: "Incorrect email or password" });
+          return;
+        }
+      } else {
+        console.log("db error: ", err);
+        res.status(500).send({ message: "login failed, please try later" });
+        return;
+      }
+    }
+  );
+});
+//-----------------------------------//
+
+///----------------LOGOUT API 3--------//
+app.post("/api/v1/logout", (req, res) => {
+  res.cookie("Token", "", {
+    maxAge: 1,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
+
+  res.send({ message: "Logout successful" });
+});
+
+//-----------------------------------------//
+//----- ADD PRODUCT API (ADMIN) 4------///
 app.post("/api/v1/product", (req, res) => {
   const body = req.body;
 
@@ -110,7 +257,7 @@ app.post("/api/v1/product", (req, res) => {
 });
 //----///
 
-//---- ADD CATEGORY API (ADMIN) ---//
+//---- ADD CATEGORY API (ADMIN) 5---//
 app.post("/api/v1/category", (req, res) => {
   const body = req.body;
 
@@ -148,7 +295,7 @@ app.post("/api/v1/category", (req, res) => {
 });
 //-----------//
 
-//----- costumerOrderApi API (user) ------///
+//----- costumerOrderApi API (user) 6------///
 app.post("/api/v1/order", (req, res) => {
   const body = req.body;
 
@@ -192,6 +339,72 @@ app.post("/api/v1/order", (req, res) => {
 });
 //---------------------------------------///
 
+//------Get All Products Api's 7----------///
+app.get("/api/v1/products", (req, res) => {
+  // const userId = new mongoose.Types.ObjectId(req.body.token._id);
+  productModel.find({}, (err, data) => {
+    if (!err) {
+      res.send({
+        message: "got all products successfully",
+        data: data,
+      });
+    } else {
+      res.status(500).send({
+        message: "server error",
+      });
+    }
+  });
+});
+//---------------------------------------//
+//------Get Costumer Order Api's 8----------///
+app.get("/api/v1/orders", (req, res) => {
+  // const userId = new mongoose.Types.ObjectId(req.body.token._id);
+  costumerOrderModel.find({}, (err, data) => {
+    if (!err) {
+      res.send({
+        message: "got all orders successfully",
+        data: data,
+      });
+    } else {
+      res.status(500).send({
+        message: "server error",
+      });
+    }
+  });
+});
+//---------------------------------------//
+///------------Uodate UserName 9----------//
+app.put("/api/v1/product/:id", async (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  if (!body.fullName) {
+    res.status(400).send({ message: "Required Parameters Are Missing" });
+    return;
+  }
+  try {
+    let data = await userModel
+      .findByIdAndUpdate(
+        id,
+        {
+          fullName: body.fullName,
+        },
+        { new: true }
+      )
+      .exec();
+    console.log("updated: ", data);
+    res.send({
+      message: "Name Updated Successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send({
+      message: "server error",
+    });
+  }
+});
+// -----------------------------------------------//
 const __dirname = path.resolve();
 app.use("/", express.static(path.join(__dirname, "./Frontend/build")));
 app.use("*", express.static(path.join(__dirname, "./Frontend/build")));
